54d7941dccdc378a129d588d0944e49786200ee9
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2016-02-12 12:43:09 +0100

    almoust done with netupgrade.sh script

--- plugins/juci-sysupgrade/scripts/netupgrade.sh
+++ plugins/juci-sysupgrade/scripts/netupgrade.sh
@@ -1,31 +1,63 @@
 #! /bin/sh
 
 send_event() {
- 	case $1 in
+	if [ "$#" -eq 3 ]; then
+		ubus send "$2" "{\"code\":\"$1\", \"status\":\"$3\" }"
+		return "$?"
+	fi
+ 	case "$1" in
 		0)
-			ubus send "$2" "{\"code\":$1, \"status\":\"Success\"}"
+			ubus send "$2" "{\"code\":\"$1\", \"status\":\"Success\"}"
 			;;
 		4)
-			ubus send "$2" "{\"code\": $1, \"status\":\"Network error\"}"
+			ubus send "$2" "{\"code\": \"$1\", \"status\":\"Network error\"}"
 			;;
 		6)
-			ubus send "$2" "{\"code\": $1, \"status\":\"Authentication error\"}"
+			ubus send "$2" "{\"code\": \"$1\", \"status\":\"Authentication error\"}"
 			;;
 		*)
-			ubus send "$2" "{\"code\": $1, \"status\":\"Unknown error\"}"
+			ubus send "$2" "{\"code\": \"$1\", \"status\":\"Unknown error\"}"
 			;;
 	esac
-	return $1
+		return "$?"
 }
 print_help() {
-	printf "Usage: netupgrade.sh test|run [options] URL\n\n"
+	printf "Usage: $0 test|start [options] URL\n\n"
 	printf "Options:\n\t-u|--user uname:\tset username for protected urls\n\t-p|--password passwd:\tset password for protected urls\n\t-s|--save:\t\tset this flag to save settings on upgrade\n\n"
+}
+
+run_test() {
+
+[ "$USER" -a "$PASS" ] && wget -q --user="$USER" --password="$PASS" --spider $URL || wget -q --spider $URL
+
+send_event "$?" "netupgrade.test"
+
+}
+
+start_download() {
+
+[ "$USER" -a "$PASS" ] && wget -q --user="$USER" --password="$PASS" -O "$IMGPATH" "$URL" || wget -q -O "$IMGPATH" "$URL"
+
+send_event $? "netupgrade.download_complete"
+
+sysupgrade -T $IMGPATH && sysupgrade $([ "$SAVE" ] || echo -n) $IMGPATH 
+
+a=$?
+
+[ $a -eq 0 ] && ret="upgrade successful" || ret="upgrade failure"
+send_event "$a" "netupgrade.sysupgrade_done" "$ret"
+
 }
 
 [ $# -lt 2 ] && print_help && return;
 
+[ "$1" != "test" -a "$1" != "start" ] && print_help
+METHOD=$1
+shift
+
 IMGPATH="/tmp/firmware.bin"
 rm -f $IMGPAT
+
 while [ $# -gt 1 ]
 	do
 	key="$1"
@@ -49,46 +81,16 @@ while [ $# -gt 1 ]
 done
 URL=$1
 
-[ "$USER" -a "$PASS" ] && wget -q --user="$USER" --password="$PASS" --spider $URL || wget -q --spider $URL
-
-case $? in
-	0)
-		echo "Success";
-		;;
-	4)
-		echo "Network error";
-		return;
-		;;
-	6)
-		echo "Authentication error";
-		return;
+case "$METHOD" in
+	start)
+		 start_download
+		 ;;
+	test)
+		run_test
 		;;
 	*)
-		echo "Unknown error";
-		return;
-		;;
+		print_help
 esac
+return 0
 
-[ "$USER" -a "$PASS" ] && wget -q --user="$USER" --password="$PASS" -O $IMGPATH $URL || wget -q -O $IMGPATH $URL
 
-case $? in
-	0)
-		echo "Success";
-		;;
-	4)
-		echo "Network error";
-		return;
-		;;
-	6)
-		echo "Authentication error";
-		return;
-		;;
-	*)
-		echo "Unknown error";
-		return;
-		;;
-esac
-
-sysupgrade -T $IMGPATH && sysupgrade $([ "$SAVE" ] || echo -n) $IMGPATH 
-
-echo $?
