61fdd2579452ef10ceafdc57c11df478ba3a0a24
Christopher Nagy <christopher.nagy@inteno.se>
2016-08-08 10:57:17 +0200

    realtime graph widget fully functional

--- juci/src/widgets/juci-realtime-graph.js
+++ juci/src/widgets/juci-realtime-graph.js
@@ -4,7 +4,10 @@ JUCI.app
 		scope: {
 			model: "=ngModel",
 			id: "@id",
-			tick: "=tick"
+			tick: "=tick",
+			min: "@min",
+			max: "@max",
+			ylabel: "@ylabel"
 		}, 
 		templateUrl: "/widgets/juci-realtime-graph.html",
 		//template: '<div id={{id}}></div>',
@@ -12,109 +15,109 @@ JUCI.app
 	}; 
 })
 .controller("juciRealtimeGraphCtrl", function($scope){
-	if(!$scope.tick){ $scope.tick = 1000; }
-	console.log("###############");
-	console.log($scope.id);
-	console.log("###############");
+	$scope.$watch("id",function(){
+		if(!$scope.id){return;}
 
-	var DELAY = $scope.tick; // delay[ms] to add new data points
-	var groups = new vis.DataSet();
-	var names = Object.keys($scope.model);
-	for(var i=0; i<names.length; i++){
-		groups.add({ id:i, content:names[i] });
-	}
-	/*
-	groups.add({
-		id: 0,
-		content: "first"
-		//options: {
-		//	shaded: {
-		//		orientation: 'top'
-		//}}
-	});
-	groups.add({
-		id: 1,
-		content: "second"
-	});
-	*/
+		if(!$scope.tick){ $scope.tick = 1000; }
+		if(!$scope.min){ $scope.min = 0; } else{ $scope.min = parseInt($scope.min); }
+		if(!$scope.max){ $scope.max = 10000; } else{ $scope.max = parseInt($scope.max); }
+		if(!$scope.ylabel){ $scope.ytitle = {}; } else{ $scope.ytitle = { "text": $scope.ylabel }; }
 
-
-	// create a graph2d with an (currently empty) dataset
-	var container = document.getElementById($scope.id);
-	var dataset = new vis.DataSet();
-	var options = {
-		start: vis.moment().add(-30, 'seconds'), // changed so its faster
-		end: vis.moment(),
-		dataAxis: {
-			left: {
-				range: {
-					min:0, max: 10000
-				}
-			}
-		},
-		drawPoints: {
-			style: 'circle' // square, circle
-		},
-		shaded: {
-			orientation: 'bottom' // top, bottom
-		},
-		legend: true
-	};
-	var graph2d = new vis.Graph2d(container, dataset, groups, options);
-
-	function renderStep() {
-		// move the window (you can think of different strategies).
-		var now = vis.moment();
-		var range = graph2d.getWindow();
-		var interval = range.end - range.start;
-		// continuously move the window
-		graph2d.setWindow(now - interval, now, {animation: false});
-		requestAnimationFrame(renderStep);
-	}
-	renderStep();
-
-        // Add a new datapoint to the graph
-	function addDataPoint() {
-		// add a new data point to the dataset
-		var now = vis.moment();
-		var datatypes = Object.keys($scope.model);
-		for(var i=0; i<datatypes.length; i++){
-			dataset.add({
-				x: now,
-				y: $scope.model[datatypes[i]],
-				group: i
-			});
+		var DELAY = $scope.tick; // delay[ms] to add new data points
+		var groups = new vis.DataSet();
+		var names = Object.keys($scope.model);
+		for(var i=0; i<names.length; i++){
+			groups.add({ id:i, content:names[i] });
 		}
-		/*dataset.add({
-			x: now,
-			y: $scope.model,//y(now / 1000) //TODO: 1000? (=DELAY=TICK?)
-			group: 1
+		/*
+		groups.add({
+			id: 0,
+			content: "first"
+			//options: {
+			//	shaded: {
+			//		orientation: 'top'
+			//}}
+		});
+		groups.add({
+			id: 1,
+			content: "second"
 		});
 		*/
 
-		// remove all data points which are no longer visible
-		var range = graph2d.getWindow();
-		var interval = range.end - range.start;
-		var oldIds = dataset.getIds({
-			filter: function (item) {
-				return item.x < range.start - interval;
+
+		// create a graph2d with an (currently empty) dataset
+		var container = document.getElementById($scope.id);
+		//container.innerHTML = container.innerHTML + $scope.id;
+		var dataset = new vis.DataSet();
+		var options = {
+			start: vis.moment().add(-30, 'seconds'), // changed so its faster
+			end: vis.moment(),
+			dataAxis: {
+				left: {
+					range: { min:$scope.min, max:$scope.max },
+					title: $scope.ytitle
+				}
+			},
+			drawPoints: {
+				style: 'circle' // square, circle
+			},
+			shaded: {
+				orientation: 'bottom' // top, bottom
+			},
+			drawPoints: false,
+			legend: true
+		};
+		var graph2d = new vis.Graph2d(container, dataset, groups, options);
+
+		function renderStep() {
+			// move the window (you can think of different strategies).
+			var now = vis.moment();
+			var range = graph2d.getWindow();
+			var interval = range.end - range.start;
+			// continuously move the window
+			graph2d.setWindow(now - interval, now, {animation: false});
+			requestAnimationFrame(renderStep);
+		}
+		renderStep();
+
+		// Add a new datapoint to the graph
+		function addDataPoint() {
+			// add a new data point to the dataset
+			var now = vis.moment();
+			var datatypes = Object.keys($scope.model);
+			for(var i=0; i<datatypes.length; i++){
+				dataset.add({
+					x: now,
+					y: $scope.model[datatypes[i]],
+					group: i
+				});
 			}
-		});
-		dataset.remove(oldIds);
 
-		setTimeout(addDataPoint, DELAY);
+			// remove all data points which are no longer visible
+			var range = graph2d.getWindow();
+			var interval = range.end - range.start;
+			var oldIds = dataset.getIds({
+				filter: function (item) {
+					return item.x < range.start - interval;
+				}
+			});
+			dataset.remove(oldIds);
 
-		// update y-axis so all datapoints are visible
-		var maxData = dataset.max("y")["y"];
-		var maxAxis = options.dataAxis.left.range.max;
-		var minData = dataset.min("y")["y"];
-		var minAxis = options.dataAxis.left.range.min;
+			setTimeout(addDataPoint, DELAY);
 
-		if(maxData > maxAxis){ options.dataAxis.left.range.max = maxData + 1 }
-		if(minData < minAxis){ options.dataAxis.left.range.min = minData - 1 }
-		graph2d.setOptions(options);
+			// update y-axis so all datapoints are visible
+			var maxData = dataset.max("y")["y"];
+			var maxAxis = options.dataAxis.left.range.max;
+			var minData = dataset.min("y")["y"];
+			var minAxis = options.dataAxis.left.range.min;
 
-	}
+			if(maxData > maxAxis){ options.dataAxis.left.range.max = maxData + 1 }
+			if(minData < minAxis){ options.dataAxis.left.range.min = minData - 1 }
+			graph2d.setOptions(options);
+
+		}
 
 	addDataPoint();
+
+	},true);
 }); 
--- plugins/juci-realtime-graphs/src/pages/juci-realtime-graphs.js
+++ plugins/juci-realtime-graphs/src/pages/juci-realtime-graphs.js
@@ -1,4 +1,13 @@
 JUCI.app.controller("rtgraphsCtrl", function($scope, $uci){
+	$scope.load = {
+		"1 m" : 0,
+		"5 m" : 0,
+		"15 m" : 0
+	};
+	$scope.connections = {
+		"udp_count" : 0,
+		"tcp_count" : 0
+	}
 	$scope.interfacesOLD = {};
 	$scope.interfacesNEW = {};
 	$scope.interfacesDIFF = {};
@@ -7,16 +16,16 @@ JUCI.app.controller("rtgraphsCtrl", function($scope, $uci){
 		 "eth2": { "rx":"0", "tx":"0"},
 		 "eth4": { "rx":"0", "tx":"0"}
 	};
-	$scope.tick = 2000;
+	$scope.tick = 1000;
 
-	$rpc.$call("router", "info").done(function(data){ //TODO: Change "info" to "interfaces"
+	$rpc.$call("router", "interfaces").done(function(data){
 		$scope.interfacesOLD = data;
 		$scope.interfacesNEW = data;
 	});
 
 	function updateInterfaces(){
 		$scope.interfacesOLD = $scope.interfacesNEW;
-		$rpc.$call("router", "info").done(function(data){ //TODO: Change "info" to "interfaces"
+		$rpc.$call("router", "interfaces").done(function(data){
 			$scope.interfacesNEW = data;
 			for(var key in data){
 				if($scope.bytes[key]){
@@ -25,10 +34,36 @@ JUCI.app.controller("rtgraphsCtrl", function($scope, $uci){
 				}
 			}
 		});
-		$scope.$apply(); //TODO: Needed?
+		$scope.$apply();
+	}
+
+	function updateLoad(){
+		function bitshift16(nr){ return nr/65535; }
+
+		$rpc.$call("system", "info").done(function(data){
+			load = data.load.map(bitshift16);
+			$scope.load = {
+				"1 m" : load[0],
+				"5 m" : load[1],
+				"15 m": load[2]
+			};
+		});
+		$scope.$apply();
+	}
+
+	function updateConnections(){
+		$rpc.$call("router", "info").done(function(data){
+			$scope.connections = {
+				"UDP" : data.system.udp_count,
+				"TCP" : data.system.tcp_count
+			};
+		});
+		$scope.$apply();
 	}
 
 	setInterval(updateInterfaces,$scope.tick);
+	setInterval(updateLoad,$scope.tick);
+	setInterval(updateConnections,$scope.tick);
 
 
 
